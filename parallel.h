021176efc19ec7a3a6a7d667c33f641cc3f44583
Ryan Abbott <Ryan@MIchaels-MacBook-Air-2.local>
2015-07-09 15:22:45 -0400

    added a header file for parallelization-related things

---
+++ parallel.h
@@ -0,0 +1,21 @@
+#ifndef __PARALLEL__
+
+#define __PARALLEL__
+
+//The generator node generates the graphs
+//and sends them to other nodes to be checked.
+//The generater sends out every regular graph,
+//possibly repeats since isomorphism checking is hard
+#define GENERATOR_RANK 0
+//the manager node manages which threads are doing what 
+//and also tells them where to send their data along
+#define MANAGER_RANK 1
+
+//each of these will perform the actions of their respective node(s)
+void generator();
+void manager();
+void distance_calculator();
+void prelim_iso_checker();
+void final_iso_checker();
+
+#endif
