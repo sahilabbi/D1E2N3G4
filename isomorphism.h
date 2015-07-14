2e3231ba360532c2612206f0f70fec27d06ca970
Ryan Abbott <Ryan@MIchaels-MacBook-Air-2.local>
2015-07-14 16:36:07 -0400

    changed around isomorphism.h

--- isomorphism.h
+++ isomorphism.h
@@ -5,8 +5,19 @@
 
 #include "graph.h"
 
-int * eigenvalues(graph_t * g);
-int * matrix_vector_mult(graph_t * g, int * v);
+typedef struct
+{
+    float values[N][N];
+} matrix;
+
+matrix * graph_to_matrix(graph_t * g);
+
+float * eigenvalues(matrix * m);
+float * matrix_vector_mult(matrix * g, float * v);
+
+//for the power iteration method
+float getEigenvalue(matrix * m);
+float remove_eigenvalue(matrix * m, float * eigenvector, float eigenvalue);
 
 bool accurate_isomorphism(graph_t * g1, graph_t * g2);
 
