1707f9249f4ba7ba3f8823545d553a98c45c5cff
Ryan Abbott <Ryan@MIchaels-MacBook-Air-2.local>
2015-08-07 16:47:11 -0400

    Removed most of the matrix stuff, which will be replaced by gsl (a library).  Rewrote most of the declarations related to isomorphisms

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 MPICC ?= mpicc 
 CFLAGS += -Wall -O3 -I .
+LDFLAGS += -lgsl
 NAME = exhuastive_search
 OBJECTS = $(patsubst %.c, %.o, $(wildcard *.c))
 
@@ -11,16 +12,16 @@ CFLAGS += -DN=$(N) -DK=$(K) -g
 all: $(NAME)
 
 increment_test: graph.c graph.h
-	gcc $(CFLAGS) -o $(NAME) -DINCREMENT_TEST graph.c
+	gcc $(CFLAGS) -o $(NAME) -DINCREMENT_TEST graph.c $(LDFLAGS)
 
 matrix_test: isomorphism.c isomorphism.h graph.h
-	gcc $(CFLAGS) -o $(NAME) -DMATRIX_TEST isomorphism.c
+	gcc $(CFLAGS) -o $(NAME) -DMATRIX_TEST isomorphism.c $(LDFLAGS)
 
 clean:
 	rm -f $(NAME) distance_test swap_test $(OBJECTS)
 
 $(NAME): $(OBJECTS)
-	$(MPICC) -g -o $@ $^ -lm
+	$(MPICC) -g -o $@ $^ $(LDFLAGS)
 
 $(OBJECTS): %.o: %.c
 	$(MPICC) $(CFLAGS) -c -o $@ $<
--- isomorphism.h
+++ isomorphism.h
@@ -4,72 +4,39 @@
 #include <stdbool.h>
 
 #include "graph.h"
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_matrix.h>
 
 
 //Used to avoid rounding errors
 bool float_equals(float a, float b);
 
+gsl_matrix * graph_to_matrix(graph_t * g);
+
+//A permutation set is a set of indices from
+//the first graph (leftSet) to the
+//second graph (rightSet)
+//size gives the number of indicies
 typedef struct
 {
-    float * values;
-    //all matrices are square, so only one size is needed
+    int * leftSet;
+    int * rightSet;
     int size;
-} matrix;
-
-matrix * create_matrix(int size);
-void delete_matrix(matrix * m);
+} PermutationSet;
 
 typedef struct
 {
-    float * values;
-    int size;
-} vector;
+    PermutationSet * permSets;
+    int num_perm_sets;
+} PartialPermutation;
 
-vector * create_vector(int size);
-void delete_vector(vector * v);
+//combines both permutations and stores the result in acc.  perm is destroyed in the process
+void combinePartialPermutations(PartialPermutation * acc, PartialPermutation * perm);
 
-typedef struct
-{
-    vector ** vectors;
-    int num_vectors;
-    //the size of the vectors in the space
-    int size;
-} vector_space;
-vector_space * create_vector_space(int num_vectors, int size);
-void delete_vector_space(vector_space * vspace);
+PartialPermutation * getPermutation(gsl_vector_view * left, gsl_vector_view * right);
 
-matrix * graph_to_matrix(graph_t * g);
+PartialPermutation * isomorphism(graph_t * g1, graph_t * g2);
 
-matrix * matrix_transpose(matrix * m);
+bool is_isomorphic(graph_t * g1, graph_t * g2);
 
-vector * eigenvalues(matrix * m);
-vector * matrix_vector_mult(matrix * g, vector * v);
-
-float dot_product(vector * v1, vector * v2);
-//multiplies the vector by a scalar to make it a unit vector
-void normailize(vector * v);
-//projects the vector v onto the space spanned by the vectors in vspace
-//vspacesize gives the number of vectors in vspace
-//normalized states whether the vectors in vspace have been normalized
-vector * project_vector(vector * v, vector_space * vspace,  bool normailized);
-//used for when the eigenspace has dimension greater that 1
-vector * angles_to_standard_basis(vector_space * vspace,  bool normalized);
-
-matrix * reduced_echelon_form(matrix * m);
-
-matrix * householder(matrix * m);
-
-typedef struct
-{
-    float * diagonal;
-    matrix * eigenvectors;
-} diagnolization;
-
-diagnolization * diagnolize(matrix * m);
-
-
-bool accurate_isomorphism(graph_t * g1, graph_t * g2);
-
-void print_matrix(matrix * m);
-void print_vector(vector * v);
 #endif
