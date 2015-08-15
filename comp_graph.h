4629a0cf6e6328a3ec337ede14b0fdbddda930e1
rabbott99 <rabbott4927@gmail.com>
2015-08-15 16:27:18 -0400

    Fixed a few definitions in comp_graph.h

--- comp_graph.h
+++ comp_graph.h
@@ -2,7 +2,9 @@
 
 #define __COMP_GRAPH__
 
-#define C ((int)(NUM_NODES / sizeof(char)))
+#include "graph.h"
+
+#define C ((int)(NUM_NODES * NUM_NODES / sizeof(char)))
 
 // graphs kept in memory or saved to disk in comp_graph format. since all will have same diameter and avg_distance, no need to save per graph 
 // this structure also used to send graph pairs across MPI channels for isomorphism checks
@@ -17,7 +19,7 @@ typedef struct
 {
 	unsigned char comp[C];
 	unsigned char	diameter;
-	float		avg_dist;
+        unsigned int 	sum_dist;
 } compress_graph;
 
 void Compress_graph(graph_t *p, comp_graph *Compress, int is_compress);
